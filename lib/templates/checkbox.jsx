9133970f012b6613e38428c6d791285659b2c202
Fabricio C Zuardi <fabricio@fabricio.org>
2015-03-17 15:32:07 -0300

    first component: checkbox

---
+++ lib/templates/checkbox.jsx
@@ -0,0 +1,33 @@
+/**
+ * Template of a HTML checkbox input element with it's label companion.
+ *
+ * Useful for creating checkboxes with custom images, where the style of the
+ * native element is set to hide it and the custom image used as the background
+ * of the label. Since the input ```id``` and the label ```for-id```
+ * attributes are set, clicking on the label background image sets the html
+ * checkbox input checked state even if it is visually hidden.
+ *
+ * @param {Object} p - the properties of the checkbox and it's label 
+ * @see mnmo-components/checkbox
+ */
+import React from 'react';
+export default (p) => {
+    return (
+<div>
+    <input type="checkbox"
+        name={p.name}
+        checked={p.checked}
+        disabled={p.disabled}
+        onChange={p.onChange}
+        id={p.id}
+        style={p.styles.input}
+    />
+    <label 
+        htmlFor={p.id} 
+        style={p.styles.label}
+    >
+        {p.children}
+    </label>
+</div>
+    );
+};
