3266a5c73029f0d4c131c69e96e7a347e7b6b547
Fabricio C Zuardi <fabricio@fabricio.org>
2015-04-27 23:41:47 -0300

    support for title in multipicker components

--- lib/templates/multipicker.jsx
+++ lib/templates/multipicker.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 
-export default (p) =>        
-<div style={p.styles.row}>
-    {p.cells.map( (cell, key) => (
-    <div key={key} style={p.styles.cell}>
-        <label>{cell.label}</label>
-        <select 
-            style={p.styles.select}
-            onChange={cell.onChange}
-            value={cell.value}
-        >
-        {cell.options.map( (option, key) => (
-            <option 
-                key={key} 
-                value={option.value}
+export default (p) => {
+    let title = (p.title) ? (
+<h2 style={p.styles.title}>
+    {p.title}
+</h2>
+    ) : null;
+    return (
+<div>
+    {title}
+    <div style={p.styles.row}>
+        {p.cells.map( (cell, key) => (
+        <div key={key} style={p.styles.cell}>
+            <label>{cell.label}</label>
+            <select 
+                style={p.styles.select}
+                onChange={cell.onChange}
+                value={cell.value}
             >
-                {option.label}
-            </option>
+            {cell.options.map( (option, key) => (
+                <option 
+                    key={key} 
+                    value={option.value}
+                >
+                    {option.label}
+                </option>
+            ))}
+            </select>
+        </div>
         ))}
-        </select>
     </div>
-    ))}
-</div>;
+</div>
+    );
+}
