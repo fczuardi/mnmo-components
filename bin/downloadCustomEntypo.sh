8a0120cc4f6be0558d05a5c71996784e42d4f1eb
Fabricio C Zuardi <fabricio@fabricio.org>
2015-09-04 11:30:09 -0300

    icon font upgraded (chart and attention glyphs)

--- bin/downloadCustomEntypo.sh
+++ bin/downloadCustomEntypo.sh
@@ -1,6 +1,6 @@
 # Entypo pictograms by Daniel Bruce — www.entypo.com
 mkdir -p ./assets/fonts/Entypo
-curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.eot -L https://fontastic.s3.amazonaws.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1431026433.eot
-curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.woff -L https://fontastic.s3.amazonaws.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1431026433.woff
-curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.ttf -L https://fontastic.s3.amazonaws.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1431026433.ttf
-curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.svg -L https://fontastic.s3.amazonaws.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1431026433.svg
+curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.eot -L https://fontastic.s3.amazonaws.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1441376341.eot
+curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.woff -L https://fontastic.s3.amazonaws.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1441376341.woff
+curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.ttf -L https://fontastic.s3.amazonaws.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1441376341.ttf
+curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.svg -L https://fontastic.s3.amazonaws.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1441376341.svg
