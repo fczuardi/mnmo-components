a7ea16a119dce0d8abe713706830fb5a667be992
Fabricio C Zuardi <fabricio@fabricio.org>
2015-12-04 12:31:30 -0200

    new icons

--- bin/downloadCustomEntypo.sh
+++ bin/downloadCustomEntypo.sh
@@ -1,6 +1,6 @@
 # Entypo pictograms by Daniel Bruce — www.entypo.com
 mkdir -p ./assets/fonts/Entypo
-curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.eot -L https://cdn.myfontastic.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1446801730.eot
-curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.woff -L https://cdn.myfontastic.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1446801730.woff
-curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.ttf -L https://cdn.myfontastic.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1446801730.ttf
-curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.svg -L https://cdn.myfontastic.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1446801730.svg
+curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.eot -L https://file.myfontastic.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1449171288.eot
+curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.woff -L https://file.myfontastic.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1449171288.woff
+curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.ttf -L https://file.myfontastic.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1449171288.ttf
+curl -o ./assets/fonts/Entypo/Custom-Entypo-mnmo.svg -L https://file.myfontastic.com/LJwoe4jsYH2RoMHM6ZwrvW/fonts/1449171288.svg
