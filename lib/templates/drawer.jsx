6d733ca8e987bc957acaf4569c43358e2e3db591
Fabricio C Zuardi <fabricio@fabricio.org>
2015-12-04 12:33:02 -0200

    saveLabel and onSaveClick options for drawer component

--- lib/templates/drawer.jsx
+++ lib/templates/drawer.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 export default (p) => {
     let backButton = (p.closeLabel) ? (
-<button style={p.styles.close} onClick={p.onCloseClick}>
-    {p.closeLabel}
-</button>
-        ) : null;
+        <button style={p.styles.close} onClick={p.onCloseClick}>
+            {p.closeLabel}
+        </button>
+    ) : null;
+    let saveButton = (p.saveLabel) ? (
+        <button style={p.styles.save} onClick={p.onSaveClick}>
+            {p.saveLabel}
+        </button>
+    ) : null;
     let header = (p.title) ? (
-<header style={p.styles.header}>
-    <h1 style={p.styles.title}>{p.title}</h1>
-    {backButton}
-</header>
-        ) : null;
+        <header style={p.styles.header}>
+            <h1 style={p.styles.title}>{p.title}</h1>
+            {backButton}
+            {saveButton}
+        </header>
+    ) : null;
     return (
 <div style={p.styles.div}>
     {header}
